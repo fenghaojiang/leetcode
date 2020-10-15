e99f7ea2be18d4459a60fd346deb46d720948c64
fenghaojiang <457420285@qq.com>
2020-10-15 13:49:27 +0800

    水题
    水题

---
+++ leetcode1281.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func subtractProductAndSum(n int) int {
+	pro := 1
+	sum := 0
+	for n > 0 {
+		pro *= n%10
+		sum += n%10
+		n /= 10
+	}
+	return pro - sum
+}
+
+func main() {
+	n := 4421
+	fmt.Println(subtractProductAndSum(n))
+}
