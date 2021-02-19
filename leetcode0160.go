bc3f2d976aefbf035b2811228c9c928ecfbd2bf7
fenghaojiang <457420285@qq.com>
2021-02-19 21:08:01 +0800

    链表

---
+++ leetcode0160.go
@@ -0,0 +1,25 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	ha, hb := headA, headB
+	for ha != hb {
+		if ha != nil {
+			ha = ha.Next
+		} else {
+			ha = headB
+		}
+		if hb != nil {
+			hb = hb.Next
+		} else {
+			hb = headA
+		}
+	}
+	return ha
+}
