4712821e9646733db545984f515a333472f9e009
fenghaojiang <457420285@qq.com>
2021-03-01 15:46:07 +0800

    更新最优解

--- leetcode0070.go
+++ leetcode0070.go
@@ -1,15 +1,9 @@
 func climbStairs(n int) int {
-	if n == 1 {
-		return 1
+	p, q, r := 0, 0, 1
+	for i := 1; i <= n; i++ {
+		p = q
+		q = r
+		r = p + q
 	}
-	if n == 2 {
-		return 2
-	}
-	stairNums := make([]int, n+1)
-	stairNums[1] = 1
-	stairNums[2] = 2
-	for i := 3; i < n+1; i++ {
-		stairNums[i] = stairNums[i-1] + stairNums[i-2]
-	}
-	return stairNums[n]
+	return r
 }
