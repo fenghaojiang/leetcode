c162e51afe51ab053817f7c24552f5423b1392c3
fenghaojiang <457420285@qq.com>
2021-04-10 14:41:07 +0800

    对称二叉树

---
+++ leetcode0101.go
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func isSymmetric(root *TreeNode) bool {
+    return check(root.Left, root.Right)
+}
+
+
+func check(p, q *TreeNode) bool {
+    if q == nil && p == nil {
+        return true
+    }
+    if q == nil && p != nil {
+        return false
+    }
+    if q != nil && p == nil {
+        return false
+    }
+    return p.Val == q.Val && check(p.Left, q.Right) && check(p.Right, q.Left)
+}
