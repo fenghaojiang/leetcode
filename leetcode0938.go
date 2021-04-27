a75538587e17a757377f5f9adb099e58b5d390e2
fenghaojiang <fenghaojiang97@m.scnu.edu.cn>
2021-04-27 13:49:06 +0800

    搜索树的深度遍历

---
+++ leetcode0938.go
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rangeSumBST(root *TreeNode, low int, high int) int {
+	if root == nil {
+		return 0
+	}
+	if root.Val >= low && root.Val <= high {
+		return root.Val + rangeSumBST(root.Left, low, high) + rangeSumBST(root.Right, low, high)
+	} else {
+		return rangeSumBST(root.Left, low, high) + rangeSumBST(root.Right, low, high)
+	}
+	return 0
+}
