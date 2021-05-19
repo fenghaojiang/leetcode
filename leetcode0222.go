3e65c41a6d0270accd260f89ba1fd319268a472c
fenghaojiang <fenghaojiang97@m.scnu.edu.cn>
2021-05-19 13:24:20 +0800

    完全二叉树，二分+位运算
    不好意思，我是年轻人，别跟我讲武德。DFS奥利给干啦

---
+++ leetcode0222.go
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+	return countNodes(root.Left) + countNodes(root.Right) + 1
+}
+
+//下面是官方解
+
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	level := 0
+	for node := root; node.Left != nil; node = node.Left {
+		level++
+	}
+	return sort.Search(1<<(level+1), func(k int) bool {
+		if k <= 1<<level {
+			return false
+		}
+		bits := 1 << (level - 1)
+		node := root
+		for node != nil && bits > 0 {
+			if bits&k == 0 {
+				node = node.Left
+			} else {
+				node = node.Right
+			}
+			bits >>= 1
+		}
+		return node == nil
+	}) - 1
+}
