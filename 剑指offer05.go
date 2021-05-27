9f69f6ebb1bc03c9b2049b223457d25e2ce91644
fenghaojiang <457420285@qq.com>
2021-05-27 23:10:32 +0800

    水题，熟悉下strings.Builder

---
+++ 剑指offer05.go
@@ -0,0 +1,11 @@
+func replaceSpace(s string) string {
+	var res strings.Builder
+	for i := range s {
+		if s[i] == ' ' {
+			res.WriteString("%20")
+		} else {
+			res.WriteByte(s[i])
+		}
+	}
+	return res.String()
+}
