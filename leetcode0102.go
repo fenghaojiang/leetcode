f248b8289e1c0b024d62f71a0afc82425c70bc54
fenghaojiang <457420285@qq.com>
2021-05-27 21:09:27 +0800

    增加队列优化写法

--- leetcode0102.go
+++ leetcode0102.go
@@ -29,3 +29,30 @@ func levelOrder(root *TreeNode) [][]int {
 	}
 	return res
 }
+
+// 队列写法
+
+func levelOrder2(root *TreeNode) [][]int {
+	res := [][]int{}
+	if root == nil {
+		return res
+	}
+	q := []*TreeNode{root}
+	for i := 0; len(q) > 0; i++ {
+		res = append(res, []int{})
+		p := []*TreeNode{}
+		for j := 0; j < len(q); j++ {
+			node := q[j]
+			res[i] = append(res[i], node.Val)
+			if node.Left != nil {
+				p = append(p, node.Left)
+			}
+			if node.Right != nil {
+				p = append(p, node.Right)
+			}
+		}
+		q = p
+	}
+	return res
+}
+
