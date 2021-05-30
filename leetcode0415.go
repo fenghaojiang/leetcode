44e57e458c8cc51bf27fadf6c48075d9a69f90e8
fenghaojiang <fenghaojiang97@m.scnu.edu.cn>
2021-05-30 15:46:28 +0800

    Create leetcode0415.go

---
+++ leetcode0415.go
@@ -0,0 +1,20 @@
+func addStrings(num1 string, num2 string) string {
+	carry := 0
+	res := ""
+	for i, j := len(num1)-1, len(num2)-1; j >= 0 || i >= 0 || carry != 0; i, j = i-1, j-1 {
+		cal := carry
+		if i >= 0 {
+			cal += int(num1[i] - '0')
+		}
+		if j >= 0 {
+			cal += int(num2[j] - '0')
+		}
+		if cal >= 10 {
+			carry = 1
+		} else {
+			carry = 0
+		}
+		res = strconv.Itoa(cal%10) + res
+	}
+	return res
+}
