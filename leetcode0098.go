604acf02dcf43beabdd7861dcea2c9f2a2ebde5d
fenghaojiang <fenghaojiang97@m.scnu.edu.cn>
2021-05-31 23:28:26 +0800

    树的深度搜索，中层遍历
    有点意思的题目

---
+++ leetcode0098.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isValidBST(root *TreeNode) bool {
+	return dfs(root, math.MinInt64, math.MaxInt64)
+}
+
+func dfs(root *TreeNode, lower, upper int) bool {
+	if root == nil {
+		return true
+	}
+	if root.Val <= lower || root.Val >= upper {
+		return false
+	}
+	return dfs(root.Left, lower, root.Val) && dfs(root.Right, root.Val, upper)
+}
