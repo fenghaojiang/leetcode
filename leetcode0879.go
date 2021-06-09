d241b03319ba2a8c060fde0577c403c67dac76ae
fenghaojiang <fenghaojiang97@m.scnu.edu.cn>
2021-06-09 22:05:22 +0800

    dp题目每天杀我

---
+++ leetcode0879.go
@@ -0,0 +1,25 @@
+func profitableSchemes(n int, minProfit int, group []int, profit []int) int {
+	const mod int = 1e9 + 7
+	dp := make([][]int, n+1)
+	for i := range dp {
+		dp[i] = make([]int, minProfit+1)
+		dp[i][0] = 1
+	}
+	for i, members := range group {
+		earn := profit[i]
+		for j := n; j >= members; j-- {
+			for k := minProfit; k >= 0; k-- {
+				dp[j][k] = (dp[j][k] + dp[j-members][max(0, k-earn)]) % mod
+			}
+		}
+	}
+	return dp[n][minProfit]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
