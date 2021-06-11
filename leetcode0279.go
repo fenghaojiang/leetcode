04079f7cc4627348d6fdafebec4f34579e537cfb
fenghaojiang <fenghaojiang97@m.scnu.edu.cn>
2021-06-11 21:44:37 +0800

    好简单的dp

---
+++ leetcode0279.go
@@ -0,0 +1,18 @@
+func numSquares(n int) int {
+	dp := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		minn := math.MaxInt32
+		for j := 1; j*j <= i; j++ {
+			minn = min(minn, dp[i-j*j])
+		}
+		dp[i] = minn + 1
+	}
+	return dp[n]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
