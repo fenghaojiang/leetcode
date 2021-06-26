79ee6eb189d415892356bb34eb0bae994ca6387b
fenghaojiang <457420285@qq.com>
2021-06-27 00:20:30 +0800

    Update leetcode5780.go

--- leetcode5780.go
+++ leetcode5780.go
@@ -1,21 +1,19 @@
 func canBeIncreasing(nums []int) bool {
-	var nums1 []int
-	var nums2 []int
+	var nums1 = nums
+	var nums2 = make([]int, len(nums))
+	copy(nums2, nums)
 	for i := 1; i < len(nums); i++ {
 		if nums[i-1] >= nums[i] {
-			nums2 = append(nums[:i-1], nums[i:]...)
-			nums1 = append(nums[:i], nums[i+1:]...)
+			nums2 = append(nums2[:i-1], nums2[i:]...)
+			nums1 = append(nums1[:i], nums1[i+1:]...)
+			break
 		}
 	}
 	return isIncreasing(nums1) || isIncreasing(nums2)
 }
 
 func isIncreasing(nums []int) bool {
-	l := len(nums)
-	if l <= 1 {
-		return true
-	}
-	for i := 1; i < l; i++ {
+	for i := 1; i < len(nums); i++ {
 		if nums[i-1] >= nums[i] {
 			return false
 		}
