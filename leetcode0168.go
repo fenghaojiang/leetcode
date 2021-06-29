dbde10409c55030a33487897149163ac9321c1c3
fenghaojiang <fenghaojiang97@m.scnu.edu.cn>
2021-06-29 10:55:07 +0800

    越来越菜了自己，多一个就不知道怎么处理

---
+++ leetcode0168.go
@@ -0,0 +1,12 @@
+func convertToTitle(columnNumber int) string {
+	res := make([]byte, 0)
+	for columnNumber > 0 {
+		columnNumber--
+		res = append(res, byte((columnNumber%26)+'A'))
+		columnNumber /= 26
+	}
+	for i, n := 0, len(res); i < n/2; i++ {
+		res[i], res[n-1-i] = res[n-1-i], res[i]
+	}
+	return string(res)
+}
