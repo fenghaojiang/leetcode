f7135cc713b2ce0e73488374dff9313f5e907a8e
fenghaojiang <fenghaojiang97@m.scnu.edu.cn>
2021-06-30 18:39:40 +0800

    龙鸣题 栈

---
+++ 剑指offer06.go
@@ -0,0 +1,13 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reversePrint(head *ListNode) []int {
+	if head == nil {
+		return nil
+	}
+	return append(reversePrint(head.Next), head.Val)
+}
