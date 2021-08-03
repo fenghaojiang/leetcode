49b89e5c810acbd32cbbc1bcbf653403e61919a8
fenghaojiang <457420285@qq.com>
2021-08-04 00:11:19 +0800

    Update leetcode0581.go

---
+++ leetcode0581.go
@@ -0,0 +1,21 @@
+func findUnsortedSubarray(nums []int) int {
+	l := len(nums)
+	maxn, minn := -math.MaxInt64, math.MaxInt64
+	right, left := -1, -1
+	for i, v := range nums {
+		if maxn > v {
+			right = i
+		} else {
+			maxn = v
+		}
+		if minn < nums[l-i-1] {
+			left = l - i - 1
+		} else {
+			minn = nums[l-i-1]
+		}
+	}
+	if right == -1 {
+		return 0
+	}
+	return right - left + 1
+}
