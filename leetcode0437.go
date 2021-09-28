cda7d5adb28b8cf7bac01f53aae558a1c47bd014
fenghaojiang <457420285@qq.com>
2021-09-29 00:02:57 +0800

    前缀和明天补补

--- leetcode0437.go
+++ leetcode0437.go
@@ -7,24 +7,22 @@
  * }
  */
 
-
- func rootSum(root *TreeNode, targetSum int) int {
-    var res int
-    if root == nil {
-        return res
-    }
-    if targetSum == root.Val {
-        res++
-    }
-    res += rootSum(root.Left, targetSum - root.Val)
-    res += rootSum(root.Right, targetSum - root.Val)
-    return res
+func rootSum(root *TreeNode, targetSum int) int {
+	var res int
+	if root == nil {
+		return res
+	}
+	if targetSum == root.Val {
+		res++
+	}
+	res += rootSum(root.Left, targetSum-root.Val)
+	res += rootSum(root.Right, targetSum-root.Val)
+	return res
 }
 
-
 func pathSum(root *TreeNode, targetSum int) int {
-    if root == nil {
-        return 0
-    }
-    return rootSum(root, targetSum) + pathSum(root.Left, targetSum) + pathSum(root.Right, targetSum)
+	if root == nil {
+		return 0
+	}
+	return rootSum(root, targetSum) + pathSum(root.Left, targetSum) + pathSum(root.Right, targetSum)
 }
