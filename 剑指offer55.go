bdfa10deaea2596284522b5aa5e41d513251ee08
fenghaojiang <fenghaojiang97@m.scnu.edu.cn>
2021-11-10 16:37:15 +0800

    Create 剑指offer55.go

---
+++ 剑指offer55.go
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	bias := depth(root.Left) - depth(root.Right)
+	if bias > 1 || bias < -1 {
+		return false
+	}
+	return isBalanced(root.Left) && isBalanced(root.Right)
+}
+
+func depth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(depth(root.Left), depth(root.Right)) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+} 
