91e02d3f1d8fa090298bdd42eaf03737d16b6b9e
fenghaojiang <457420285@qq.com>
2021-11-23 21:59:07 +0800

    模拟

---
+++ 剑指offerII002.go
@@ -0,0 +1,28 @@
+func addBinary(a string, b string) string {
+    ans := ""
+    carry := 0
+    lenA, lenB := len(a), len(b)
+    n := max(lenA, lenB)
+
+    for i := 0; i < n; i++ {
+        if i < lenA {
+            carry += int(a[lenA-i-1] - '0')
+        }
+        if i < lenB {
+            carry += int(b[lenB-i-1] - '0')
+        }
+        ans = strconv.Itoa(carry%2) + ans
+        carry /= 2
+    }
+    if carry > 0 {
+        ans = "1" + ans
+    }
+    return ans
+}
+
+func max(x, y int) int {
+    if x > y {
+        return x
+    }
+    return y
+}
