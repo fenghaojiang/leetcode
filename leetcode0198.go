807caf661ce576dbf942a15ce94f4078ed0c96e5
fenghaojiang <fenghaojiang97@m.scnu.edu.cn>
2021-11-30 15:05:38 +0800

    典中典动态规划

---
+++ leetcode0198.go
@@ -0,0 +1,20 @@
+func rob(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	first, second := nums[0], max(nums[0], nums[1])
+	for i := 2; i < len(nums); i++ {
+		first, second = second, max(first+nums[i], second)
+	}
+	return second
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
