3b98a138beff1702f4dde39f15f1b9bd44135b5b
fenghaojiang <fenghaojiang97@m.scnu.edu.cn>
2022-04-21 10:12:02 +0800

    Update leetcode0824.go

--- leetcode0824.go
+++ leetcode0824.go
@@ -5,8 +5,27 @@ import (
 	"strings"
 )
 
-func toGoatLatin(S string) string {
-
+func toGoatLatin(sentence string) string {
+	var vowels = map[byte]struct{}{'a': {}, 'e': {}, 'i': {}, 'o': {}, 'u': {}, 'A': {}, 'E': {}, 'I': {}, 'O': {}, 'U': {}}
+	ans := &strings.Builder{}
+	for i, cnt, n := 0, 1, len(sentence); i < n; i++ {
+		if cnt > 1 {
+			ans.WriteByte(' ')
+		}
+		start := i
+		for i++; i < n && sentence[i] != ' '; i++ {
+		}
+		cnt++
+		if _, ok := vowels[sentence[start]]; ok {
+			ans.WriteString(sentence[start:i])
+		} else {
+			ans.WriteString(sentence[start+1 : i])
+			ans.WriteByte(sentence[start])
+		}
+		ans.WriteByte('m')
+		ans.WriteString(strings.Repeat("a", cnt))
+	}
+	return ans.String()
 }
 
 func main() {
